cb675372efac5c7e6533de3d8c7535a573033c7f
Trond Trosterud <trond.trosterud@uit.no>
2008-01-28 19:36:30 +0000

    Had a look at the quasicode, removed irrelevant stuff.

--- script/overfoer.pl
+++ script/overfoer.pl
@@ -2,102 +2,57 @@
 #
 # kvasikode
 #
-# Felt:
-# # fkvnob (denne berre for referanse)
-# #"Lemma","POS","trans","tPOS","stamme1","stamme2","syntrans","syntransPOS","syntrans2","syntrans2POS","syntrans3","syntrans3POS","syneks","syneks2","syneks3"
+# smenob dtd
+# <!ELEMENT r (e+) >
+# <!ELEMENT e (lg,mg+)>
+# <!ELEMENT lg (l,lc*)>
+# <!ELEMENT l (#PCDATA)>
+# <!ATTLIST l 
+#       pos ( a | n | v | adv | po | pr | pron | i | pcle | num | cc | cs | npl | x ) #REQUIRED
+# >      
 # 
-# # nobfkv
-# #"Lemma;POS;trans;stamme1;stamme2;eks;syn1;syn2"
+# <!ELEMENT lc (#PCDATA)>
 # 
-# Les inn felta
+# <!ELEMENT mg (tg+)>
+# <!ELEMENT tg (t+,xg*)>
 # 
-# Putt relevante felt inn i relevante stader i xml-strukturen
-# 
-#   <entry>
-#     <lemma decl="POS">Lemma</lemma>
-#     <mgr>
-#       <trgr>
-#         <trans decl="x">trans</trans>
-#         <stem>stamme1</stem>
-#         <stem>stamme2</stem>
-#         <exgr>
-#           <ex>eks</ex>
-#           <extr></extr>
-#         </exgr>
-#       </trgr>
-#     </mgr>
-#     <syngr>
-#       <syn>syn1</syn>
-#       <syn>syn2</syn>
-#     </syngr>
-#  </entry>      
+# <!ELEMENT t (#PCDATA)>
+# <!ATTLIST t 
+#       pos ( a | a5 | S | m | f | n | v | adv | p | pron | i | cc | cs | im | mpl | fpl | npl | x ) #REQUIRED
+# >      >
+# <!ELEMENT xg ((x,xt?)+)>
+# <!ELEMENT x (#PCDATA)>
+# <!ELEMENT xt (#PCDATA)>
+
+# Quasicode
+
+# For each <e>, for each <t> 
+##  make the translation into lemma, 
+##  make the lemma into translation,
+##  keep the POS information on each of them
+##  keep the example group, but switch <x> and <xt>
+
+# A bit more specific
+
+# For each element <e>, do the following
+# For each node <t>, 
+# Write <e>\n<lg>\n<l POS="
+# Insert POS attr of source <t>
+# Write ">
+# Insert content of source <t>
+# Write </l>\n</lg>\n<mg>\n<tg>\n<t POS="
+# Insert POS attr of source <l>
+# Write ">
+# Insert content of source <l>
+# Write </t>\n
+# If there is <xg>
+# Write <xg>\n\<x>
+# Insert content of source <xt>
+# Write </x>\n<xt>
+# Insert content of source <x>
+# Write </xt>\n</xg>n</mg>\n<\e>\n
+# If there is no <xg>
+# Write # </tg>\n</mg>\n<\e>\n
 
 ########################################################
 
-use encoding 'utf-8';
-
-# File and directory variables:
-$SRCDIR = ".";
-
-$CSVF = $SRCDIR . "/nobfkv.xml";
-#$CSVF = $SRCDIR . "/test.csv";
-$DICT = $SRCDIR . "/dictionary.xml";
-
-open CSVF or die "Can't find file $CSVF: $!\n";
-open DICT, ">$DICT" or die "Can't find file $DICT: $!\n";
-
-print DICT "<dictionary >\n";
-
-$line = <CSVF> ;
-
-while ( $line = <CSVF> ) {
-	chomp $line;
-	($Lemma,
-     $POS,
-     $trans,
-     $stamme1,
-     $stamme2,
-     $eks,
-     $syn1)
-	   = split /"?,"?/, $line ;
-#    print "Lina er: $line\n";
-#    print "Lemma er: $Lemma\n";
-    $Lemma =~ s/"\W?(\w)/$1/; # rens $Lemma for " og ^L
-    print DICT "\t<entry>\n";
-    print DICT "\t  <lemma decl=\"$POS\">$Lemma</lemma>\n";
-    print DICT "\t  <mgr>\n";
-    print DICT "\t    <trgr>\n";
-    print DICT "\t      <trans decl=\"$POS\">$trans</trans>\n";
-    if ($stamme1 =~ /\w/) { print DICT "\t  <stem>$stamme1</stem>\n"; }
-    if ($stamme2 =~ /\w/) { print DICT "\t  <stem>$stamme2</stem>\n"; }
-    if ($eks =~ /\w/) { print DICT "\t        <exgr>\n          <ex>$eks</ex>\n          <extr></extr>\n        </exgr>\n";   }
-    print DICT "\t    </trgr>\n";
-    print DICT "\t  </mgr>\n";
-    if ($syn1 =~ /\w/) { print DICT "\t      <syngr>\n          <syn>$syn1</syn>\n         </syngr>\n"; }
-#    if ($syn2 =~ /\w/) { print DICT "\t        <syn>$syn2</syn>\n"; }
-#    if ($syn1 =~ /\w/) { print DICT "\t      </syn>\n"; }
-#    }
-#    print DICT "\t    </trgr>\n";
-    print DICT "\t</entry>\n";
-}
-
-print DICT "</dictionary>\n";
-close DICT;
-close CSVF;
-
-
-# Note: Desse blir feil og skal fiksast:
-# Dei blir merka som exgr, skal vers syneks
-# "levoton paikka","louhikkonen",
-# "tykköö","Hän oon minun tykkönä",
-# "tykköö","Hän oon minun tykkönä",
-# "himmee","Himmi sää oon umpu sää",
-# "himmee","Himmi sää oon umpu sää",
-# "tykönä","Hän oon minun tykköö",
-# "tykönä","Hän oon minun tykköö",
-# 
-# "Nahka kun menee ",,
-# "Tehe nyt sen valmhiiksi!",,
-# "Tehe nyt sen valmhiiksi!",,
-# "Ole vaiti!",,
-# 
