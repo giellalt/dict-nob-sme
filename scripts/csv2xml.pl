067ecfa28731a9c7630a33c1c025478fdf52d7e1
Trond Trosterud <trond.trosterud@uit.no>
2017-06-21 08:22:28 +0000

    Meir dokumentasjon

--- scripts/csv2xml.pl
+++ scripts/csv2xml.pl
@@ -2,7 +2,15 @@
 use utf8 ;
 
 # Simple script to convert csv to xml
-# For input/outpus examples, see below.
+# Input is expected to be of the format 
+# lemma<tab>POS<tab>translation or
+# lemma<tab>POS<tab>translation<tab>translation etc.
+# The script does not have any if-so parts, so make sure
+# your input has an equal number of translations (e.g. always 1)
+# To get 2 or 3: use the lines with 2 and 3 instead.
+
+# Command:
+# cat inputfile | perl csv2xml.pl > output
 
 
 print STDOUT "<r>\n";
@@ -10,8 +18,9 @@ print STDOUT "<r>\n";
 while (<>) 
 {
 	chomp ;
-	my ($lemma, $POS, $trans, $trans2, $trans3) = split /\t/ ;
-#	my ($lemma, $POS, $trans) = split /\t/ ;
+	my ($lemma, $POS, $trans) = split /\t/ ;
+#	my ($lemma, $POS, $trans, $trans2) = split /\t/ ;
+#	my ($lemma, $POS, $trans, $trans2, $trans3) = split /\t/ ;
 	print STDOUT "   <e usage=\"vd\">\n";
 	print STDOUT "      <lg>\n";
 	print STDOUT "         <l pos=\"$POS\">$lemma</l>\n";
@@ -19,7 +28,7 @@ while (<>)
 	print STDOUT "      <mg>\n";
 	print STDOUT "         <tg>\n";
 	print STDOUT "            <t pos=\"$POS\">$trans</t>\n";
-	print STDOUT "            <t pos=\"$POS\">$trans2</t>\n";
+#	print STDOUT "            <t pos=\"$POS\">$trans2</t>\n";
 #	print STDOUT "            <t pos=\"$POS\">$trans3</t>\n";
 	print STDOUT "         </tg>\n";
 	print STDOUT "      </mg>\n";
@@ -31,7 +40,7 @@ print STDOUT "</r>\n";
 
 
 
-# Example input:
+# Example input (_ instead of <tab>):
 #
 # се̄ййп_N_ANIMAL_хвост длинный, длинный хвост
 # кӣдтжэва_N_ANIMAL, LIVING-PLACE_животное домашнее, домашнее животное
